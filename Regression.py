3976f47bb7b95d3267d5e8f2f16d03be56868fbf
saw9090 <128577170+saw9090@users.noreply.github.com>
2024-03-16 11:16:40 +0900

    Add files via upload

--- Regression.py
+++ Regression.py
@@ -3,72 +3,102 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import statsmodels.api as sm
 
 st.title('Linear Regression')
 st.header('Data Generation')
-np.random.seed(0)
-N = st.select_slider(
-    r'Select $\N$',
-    options=[2,10,100,1000])
+st.latex(r'''Model : y_i = \beta_0 + \beta_1 x_i + \epsilon_i,\quad \epsilon_i \sim N(0,\sigma^2) \; and \; i=1,\cdots,N''') 
 
-e = np.random.normal(size=N)
-X = np.random.uniform(0,10,N)
-
-st.header('Linear Regression')
-
+# 변수 선택
 beta_0 = st.select_slider(
     r'Select True $\beta_0$',
-    options=np.linspace(0,10,101))
+    options=np.linspace(-3,3,61)
+    ,value=0)
 
 beta_1 = st.select_slider(
     r'Select True $\beta_1$',
-    options=np.linspace(0,10,101))
+    options=np.linspace(-3,3,61)
+    ,value=1)
 
-epsilon = st.select_slider(
-    r'Select True $\epsilon$',
-    options=np.linspace(0,10,101))
+sigma = st.select_slider(
+    r'Select True $\sigma$',
+    options=np.linspace(0,4,41)
+    ,value=1)
 
-import statsmodels.api as sm
-y = beta_0 + beta_1 * X + epsilon * e
-y_mean = np.mean(y)
-y_center = y-y_mean
-dfX = pd.DataFrame(X, columns=["x"])
-dfX = sm.add_constant(dfX)
-dfy = pd.DataFrame(y_center, columns=["y"])
-df = pd.concat([dfX, dfy], axis=1)
+N = st.select_slider(
+        r'Select $\N$',
+        options=[5,10,50,100,500,1000]
+        ,value=100)
+
+# data 생성
+if st.button('Data generate'):
+    e = np.random.normal(size=N)
+    if 'X' not in st.session_state:
+        st.session_state['X'] = np.random.uniform(-5,5,N)
+    if 'y' not in st.session_state:
+        st.session_state['y'] = beta_0 + beta_1 * st.session_state['X'] + sigma * e
+    st.session_state['X'] = np.random.uniform(-5,5,N)
+    st.session_state['y'] = beta_0 + beta_1 * st.session_state['X'] + sigma * e
+    fig, ax = plt.subplots()
+    sns.scatterplot(x=st.session_state['X'],y=st.session_state['y'],ax=ax,s=50, marker='x',color = 'black')
+    ax.set_title('Linear Regression')
+    ax.set_xlim([-6,6])
+    ax.set_ylim([-20,20])
+    st.pyplot(fig)
+else :  st.write('Click button!')
+
+st.header('Regression')
+
+from sklearn.linear_model import ElasticNet, LinearRegression
 
 option = st.selectbox(
     'Model Selection',
     ('Linear Regression', 'Ridge', 'Lasso','Elastic Net')
 )
-st.write('선택한 옵션:', option)
-model = sm.OLS.from_formula("y ~ x", data = df)
 if option == 'Linear Regression' :
-    result = model.fit()
+    model = LinearRegression()
 elif option == 'Ridge' :
-    lambda_ = st.select_slider(
-        r'Select $\lambda$',
+    alpha_ = st.select_slider(
+        r'Select $\alpha$',
         options=np.logspace(-3,5,8))
-    result = model.fit_regularized(alpha=lambda_,L1_wt=0)
+    model = ElasticNet(alpha = alpha_,l1_ratio=0)
 elif option == 'Lasso' :
-    lambda_ = st.select_slider(
-        r'Select $\lambda$',
+    alpha_ = st.select_slider(
+        r'Select $\alpha$',
         options=np.logspace(-3,5,8))
-    result = model.fit_regularized(alpha=lambda_,L1_wt=1)
+    model = ElasticNet(alpha = alpha_,l1_ratio=1)
 elif option == 'Elastic Net' :
-    lambda_ = st.select_slider(
-        r'Select $\lambda$',
+    alpha_ = st.select_slider(
+        r'Select $\alpha$',
         options=np.logspace(-3,5,8))
-    L1_wt = st.select_slider(
-        'Select $L1_wt$',
+    L1_ratio = st.select_slider(
+        'Select ' r'$L_1 \; ratio$',
         options=np.linspace(0,1,10))
-    result = model.fit_regularized(alpha=lambda_,L1_wt=L1_wt)
+    model = ElasticNet(alpha = alpha_,l1_ratio=L1_ratio)
 
-grid = np.linspace(min(X),max(X),N)
-dfxx = pd.DataFrame(grid, columns=["x"])
 
+X = st.session_state['X'].reshape(-1,1)
+y = st.session_state['y'].reshape(-1,1)
+model.fit(X,y)
+
+grid = np.linspace(-6,6,num = N)
 fig, ax = plt.subplots()
-sns.scatterplot(x=X,y=y,ax=ax,s=50, color ='black')
-sns.lineplot(x=grid,y=result.predict(dfxx)+y_mean,ax=ax,color='red')
-ax.set_title('Linear Regression')
+y_line = np.squeeze(model.predict(grid.reshape(-1,1)))
+sns.scatterplot(x=st.session_state['X'],y=st.session_state['y'],ax=ax,s=50, marker='x',color = 'black')
+y_mean = np.mean(st.session_state['y']) *np.ones(N)
+line1, = ax.plot(grid,y_mean, color='black') # y mean
+line2, = ax.plot(grid,y_line, color='red') # 회귀선
+line3, = ax.plot(grid,beta_0 + beta_1 * grid, color='blue') # true line
+legend_1 = ax.legend(handles=(line1,line2,line3),labels=(r'$\bar{y}$',option,'True line'),loc= 'upper right',fontsize = 'small')
+ax.add_artist(legend_1)
+beta_hat_0 = np.round(model.intercept_[0],5)
+beta_hat_1 = np.round(model.coef_[0],5)
+legend_2 = ax.legend(handles=(line1,line2,line3),
+                     labels=(r'$\bar{y}$'+' = {}'.format(np.round(np.mean(st.session_state['y']),5)),
+                             r'$\hat{\beta}_0$'+' = {}, '.format(beta_hat_0) + r'$\hat{\beta}_1$'+' = {}'.format(beta_hat_1),
+                             r'$\beta_0$ = {}, '.format(beta_0) + r'$\beta_1$ = {}'.format(beta_1)),
+                             loc = 'lower right',fontsize = 'small') # 추정량 비교
+ax.set_title(option)
+ax.set_xlim([-6,6])
+ax.set_ylim([-20,20])
 st.pyplot(fig)
