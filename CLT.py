47f77ea8a4b12fa4598410711e800dc615c23762
김기범 <128577170+saw9090@users.noreply.github.com>
2024-03-29 14:38:33 +0900

    Add files via upload

--- CLT.py
+++ CLT.py
@@ -4,8 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import norm,chi2
+import plotly.graph_objs as go
 
 st.title('Central Limit Theorem')
+#  중심극한정리 설명
 st.header('Central Limit Theorem(CLT)')
 st.write('Let '+
         r'$X_1, X_2, \cdots, X_n$'+
@@ -21,21 +23,24 @@ st.write('Let '+
 st.latex(r'''\frac{\bar{X}_n - \mu}{\sigma/\sqrt{n}} \xrightarrow{d} N(0,1)
 ''')
 
-st.header('Data Generation')
+# 옵션 선택
+with st.sidebar :
+    st.header('Data Generation')
+    N = st.select_slider(
+            r'Select $n$',
+            options=[10,100,500,1000]
+            ,value=100)
+    ns = st.select_slider(
+            'Select the number of sample means ' r'$NS$',
+            options=[10,100,500,1000]
+            ,value=100)
+    option = st.selectbox(
+        'Distribution Selection',
+        ('Uniform Distribution', 'Normal Distribution', 'Chi-squared Distribution',)
+    )
 
-N = st.select_slider(
-        r'Select $n$',
-        options=[10,100,500,1000]
-        ,value=100)
-ns = st.select_slider(
-        'Select the number of sample means ' r'$NS$',
-        options=[10,100,500,1000]
-        ,value=100)
-option = st.selectbox(
-    'Distribution Selection',
-    ('Uniform Distribution', 'Normal Distribution', 'Chi-squared Distribution',)
-)
-
+# 데이터 생성
+st.header(option)
 if option == 'Uniform Distribution' :
     st.latex(r'''X \sim U(-a,a)''')
     a = st.select_slider(
@@ -48,8 +53,11 @@ if option == 'Uniform Distribution' :
     grid2 = np.linspace(min(sample_mean),max(sample_mean),1000)
     approx = norm.pdf(grid2,0,a/(3*N)**(1/2))
 
-    fig, ax = plt.subplots(1,2, figsize=(8,3))
-    ax[0].plot(grid1,1/a* np.ones(1000),color='black')
+    with st.sidebar :
+        fig, ax = plt.subplots()
+        ax.plot(grid1, 1/a* np.ones(1000), color = 'black')
+        ax.set_title(option)
+        st.pyplot(fig)
 elif option == 'Normal Distribution' :
     st.latex(r'''X \sim N(\mu,\sigma^2)''')
     mu = st.select_slider(
@@ -65,9 +73,11 @@ elif option == 'Normal Distribution' :
     grid1 = np.linspace(mu-3*sigma,mu+3*sigma,1000)
     grid2 = np.linspace(min(sample_mean),max(sample_mean),1000)
     approx = norm.pdf(grid2,mu,sigma/(N)**(1/2))
-
-    fig, ax = plt.subplots(1,2, figsize=(8,3))
-    ax[0].plot(grid1,norm.pdf(grid1,mu,sigma),color='black')
+    with st.sidebar :
+        fig, ax = plt.subplots()
+        ax.plot(grid1, approx, color = 'black')
+        ax.set_title(option)
+        st.pyplot(fig)
 elif option == 'Chi-squared Distribution' :
     st.latex(r'''X \sim \chi^2(k)''')
     df = st.select_slider(
@@ -80,17 +90,49 @@ elif option == 'Chi-squared Distribution' :
     grid2 = np.linspace(df-3*(2*df/N)**(1/2),df+3*(2*df/N)**(1/2),1000)
     approx = norm.pdf(grid2,df,(2*df/N)**(1/2))
 
-    fig, ax = plt.subplots(1,2, figsize=(8,3))
-    ax[0].plot(grid1,chi2.pdf(grid1,df),color='black')
+    with st.sidebar :
+        fig, ax = plt.subplots()
+        ax.plot(grid1, chi2.pdf(grid1,df), color = 'black')
+        ax.set_title(option)
+        st.pyplot(fig)
+if st.button('Data Regenerate'):
+    pass
 
-ax[0].set_title(option)
-sns.histplot(sample_mean,stat='density',kde=True,ax=ax[1],color='blue',label='Sample Histogram of '+r'$\bar{X}$')
-ax[1].plot(grid2,approx,color='black',label = 'Distribution by CLT')
-ax[1].set_title('Approximate Distribution')
-ax[1].legend(loc='upper right', bbox_to_anchor=(1.8,1.0))
+fig, axs = plt.subplots(3, 3, figsize=(15, 15))
+
+axs[0, 0].axvline(mat[:, 0].mean(), color='red', linestyle='--',linewidth = 2.5 ,label = 'mean')
+axs[0, 1].axvline(mat[:, 1].mean(), color='green', linestyle='--',linewidth = 2.5 ,label = 'mean')
+axs[0, 2].axvline(mat[:, 2].mean(), color='blue', linestyle='--',linewidth = 2.5 ,label = 'mean')
+
+for i in range(3):
+    sns.histplot(mat[:, i], kde=True, ax=axs[0, i], color='black')
+    if option == 'Uniform Distribution':
+        axs[0, i].set_xticks([-a-1,0,a+1])
+    axs[0, i].set_title(f'Sample {i+1} Histogram')
+    axs[0, i].legend(fontsize = 15)
+
+
+ax_big = plt.subplot2grid((3,3),(1,0),rowspan = 2, colspan = 3)
+sns.histplot(sample_mean, kde=True, ax=ax_big, color='gray')
+ax_big.axvline(sample_mean[0], color='red', linestyle='-',linewidth = 2.5, label = '1st Sample Mean')
+ax_big.axvline(sample_mean[1], color='green', linestyle='-',linewidth = 2.5, label = '2nd Sample Mean')
+ax_big.axvline(sample_mean[2], color='blue', linestyle='-',linewidth = 2.5, label = '3rd Sample Mean')
+ax_big.legend(fontsize = 15)
+ax_big.set_title('Distribution of Sample Means')
+
+
+axs[0, 0].set_title('First Sample')
+axs[0, 1].set_title('Second Sample')
+axs[0, 2].set_title('Third Sample')
+
+plt.tight_layout()
 st.pyplot(fig)
 
-if st.button('Data Regeneration'):
-    pass
+# ax[0].set_title(option)
+# sns.histplot(sample_mean,stat='density',kde=True,ax=ax[1],color='blue',label='Sample Histogram of '+r'$\bar{X}$')
+# ax[1].plot(grid2,approx,color='black',label = 'Distribution by CLT')
+# ax[1].set_title('Approximate Distribution')
+# ax[1].legend(loc='upper right', bbox_to_anchor=(1.8,1.0))
+
 
 
--- Regression.py
+++ Regression.py
@@ -3,15 +3,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import plotly.graph_objects as go
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
 from sklearn.linear_model import ElasticNet, LinearRegression
+
 
 st.title('Linear Regression')
 
 st.latex(r'''Model : y_i = \beta_0 + \beta_1 x_i + \epsilon_i,\quad \epsilon_i \sim N(0,\sigma^2) \; and \; i=1,\cdots,N''') 
 
 # 변수 선택
-
 with st.sidebar :
     st.header('Data Generation')
     beta_0 = st.select_slider(
@@ -33,7 +34,9 @@ with st.sidebar :
         r'Select $\N$',
         options=[5,10,50,100,500,1000]
         ,value=100)
-    if st.button('Regenerate Data'):
+    if 'count' not in st.session_state:
+        st.session_state['count'] = 0
+    if st.session_state['count'] == 0 :
         e = np.random.normal(size=N)
         if 'X' not in st.session_state:
             st.session_state['X'] = np.random.uniform(-5,5,N)
@@ -47,45 +50,65 @@ with st.sidebar :
         ax.set_xlim([-6,6])
         ax.set_ylim([-20,20])
         st.pyplot(fig)
+        st.session_state['count'] += 1
+        
+    if st.button('Generate Data'):
+        e = np.random.normal(size=N)
+        if 'X' not in st.session_state:
+            st.session_state['X'] = np.random.uniform(-5,5,N)
+        if 'y' not in st.session_state:
+            st.session_state['y'] = beta_0 + beta_1 * st.session_state['X'] + sigma * e
+        st.session_state['X'] = np.random.uniform(-5,5,N)
+        st.session_state['y'] = beta_0 + beta_1 * st.session_state['X'] + sigma * e
+        fig, ax = plt.subplots()
+        sns.scatterplot(x=st.session_state['X'],y=st.session_state['y'],ax=ax,s=50, marker='x',color = 'black')
+        ax.set_title('Data')
+        ax.set_xlim([-6,6])
+        ax.set_ylim([-20,20])
+        st.pyplot(fig)
+        st.write('Done!')
     else : 
-        e = np.random.normal(size=N)
-        if 'X' not in st.session_state:
-            st.session_state['X'] = np.random.uniform(-5,5,N)
-        if 'y' not in st.session_state:
-            st.session_state['y'] = beta_0 + beta_1 * st.session_state['X'] + sigma * e
-        st.session_state['X'] = np.random.uniform(-5,5,N)
-        st.session_state['y'] = beta_0 + beta_1 * st.session_state['X'] + sigma * e
-        fig, ax = plt.subplots()
-        sns.scatterplot(x=st.session_state['X'],y=st.session_state['y'],ax=ax,s=50, marker='x',color = 'black')
-        ax.set_title('Data')
-        ax.set_xlim([-6,6])
-        ax.set_ylim([-20,20])
-        st.pyplot(fig)
-
+        pass
 
 option = st.selectbox(
     'Model Selection',
     ('Linear Regression', 'Ridge', 'Lasso','Elastic Net')
 )
 if option == 'Linear Regression' :
+    st.latex(r'''Loss(\beta_0,\beta_1) = \sum^{N}_{i=1}\left(y_i-(\beta_0 + \beta_1 x_{i})\right)^2''')
+    st.latex(r'''\hat{\beta}_0,\hat{\beta}_1 = \argmin_{\beta_0, \beta_1} Loss(\beta_0,\beta_1)''')
+elif option == 'Ridge' :
+    st.latex(r'''Loss(\beta_0,\beta_1) = \sum^{N}_{i=1}\left(y_i-(\beta_0 + \beta_1 x_{i})\right)^2 + \alpha\beta_1^2   ''')
+    st.latex(r'''\hat{\beta}_0,\hat{\beta}_1 = \argmin_{\beta_0, \beta_1} Loss(\beta_0,\beta_1)''')
+elif option == 'Lasso' :
+    st.latex(r'''Loss(\beta_0,\beta_1) = \sum^{N}_{i=1}\left(y_i-(\beta_0 + \beta_1 x_{i})\right)^2 + \alpha|\beta_1|   ''')
+    st.latex(r'''\hat{\beta}_0,\hat{\beta}_1 = \argmin_{\beta_0, \beta_1} Loss(\beta_0,\beta_1)''')
+elif option == 'Elastic Net' :
+    st.latex(r'''Loss(\beta_0,\beta_1) = \sum^{N}_{i=1}\left(y_i-(\beta_0 + \beta_1 x_{i})\right)^2 + \alpha(L_1|\beta_1| + \frac{1-L_1}{2}\beta_1^2)   ''')
+    st.latex(r'''\hat{\beta}_0,\hat{\beta}_1 = \argmin_{\beta_0, \beta_1} Loss(\beta_0,\beta_1)''')
+
+
+
+if option == 'Linear Regression' :
     model = LinearRegression()
 elif option == 'Ridge' :
     alpha_ = st.select_slider(
         r'Select $\alpha$',
-        options=np.logspace(-3,5,8))
+        options=np.round(np.logspace(-3,5,8),5))
     model = ElasticNet(alpha = alpha_,l1_ratio=0)
 elif option == 'Lasso' :
     alpha_ = st.select_slider(
         r'Select $\alpha$',
-        options=np.logspace(-3,5,8))
+        options=np.round(np.logspace(-3,5,8),5))
     model = ElasticNet(alpha = alpha_,l1_ratio=1)
 elif option == 'Elastic Net' :
     alpha_ = st.select_slider(
         r'Select $\alpha$',
-        options=np.logspace(-3,5,8))
+        options=np.round(np.logspace(-3,5,8),5))
     L1_ratio = st.select_slider(
         'Select ' r'$L_1 \; ratio$',
-        options=np.linspace(0,1,10))
+        options=np.round(np.linspace(0,1,11),5),
+        value= 0.5)
     model = ElasticNet(alpha = alpha_,l1_ratio=L1_ratio)
 
 
@@ -93,6 +116,7 @@ X = st.session_state['X'].reshape(-1,1)
 y = st.session_state['y'].reshape(-1,1)
 model.fit(X,y)
 beta_hat_0 = np.round(model.intercept_[0],5)
+
 if option == 'Linear Regression' :
     beta_hat_1 = np.round(model.coef_[0],5)[0]
 else :
@@ -103,21 +127,47 @@ y_line = np.squeeze(model.predict(grid.reshape(-1,1)))
 y_mean = np.mean(st.session_state['y']) *np.ones(N)
 
 
-fig, ax = plt.subplots()
+fig = go.Figure()
 
-sns.scatterplot(x=st.session_state['X'],y=st.session_state['y'],ax=ax,s=50, marker='x',color = 'black')
-line1, = ax.plot(grid,y_mean, color='black') # y mean
-line2, = ax.plot(grid,y_line, color='red') # 회귀선
-line3, = ax.plot(grid,beta_0 + beta_1 * grid, color='blue') # true line
-legend_1 = ax.legend(handles=(line1,line2,line3),labels=(r'$\bar{y}$',option,'True line'),loc= 'upper right',fontsize = 'small')
-ax.add_artist(legend_1)
-legend_2 = ax.legend(handles=(line1,line2,line3),
-                     labels=(r'$\bar{y}$'+' = {}'.format(np.round(np.mean(st.session_state['y']),5)),
-                             r'$\hat{\beta}_0$'+' = {}, '.format(beta_hat_0) + r'$\hat{\beta}_1$'+' = {}'.format(beta_hat_1),
-                             r'$\beta_0$ = {}, '.format(beta_0) + r'$\beta_1$ = {}'.format(beta_1)),
-                             loc = 'lower right',fontsize = 'small') # 추정량 비교
+fig.add_trace(go.Scatter(x=st.session_state['X'],
+                         y=st.session_state['y'],
+                         mode = 'markers',
+                         marker = dict(color = 'black', size = 5, symbol = 'x'),
+                         name = 'Data'))
+fig.add_trace(go.Scatter(x = grid,
+                         y = y_mean,
+                         mode = 'lines',
+                         line = dict(color = 'black'),
+                         name = 'y̅ = {}'.format(np.round(np.mean(st.session_state['y']),5))+ ' (Sample mean of y)'))
+fig.add_trace(go.Scatter(x = grid,
+                         y = y_line,
+                         mode = 'lines',
+                         line = dict(color = 'red'),
+                         name = ' β̂₀ = {}, '.format(beta_hat_0) +'β̂₁ = {}'.format(beta_hat_1)+ ' ({})'.format(option))) 
+fig.add_trace(go.Scatter(x = grid,
+                         y = beta_0 + beta_1 * grid,
+                         mode = 'lines',
+                         line = dict(color = 'blue'),
+                         name = 'β₀ = {}, '.format(beta_0) + 'β₁ = {}'.format(beta_1) + ' (True line)'))
+st.plotly_chart(fig)
 
-ax.set_title(option)
-ax.set_xlim([-6,6])
-ax.set_ylim([-20,20])
-st.pyplot(fig)
+
+## matplotlib
+# fig, ax = plt.subplots()
+
+# sns.scatterplot(x=st.session_state['X'],y=st.session_state['y'],ax=ax,s=50, marker='x',color = 'black')
+# line1, = ax.plot(grid,y_mean, color='black') # y mean
+# line2, = ax.plot(grid,y_line, color='red') # 회귀선
+# line3, = ax.plot(grid,beta_0 + beta_1 * grid, color='blue') # true line
+# legend_1 = ax.legend(handles=(line1,line2,line3),labels=(r'$\bar{y}$',option,'True line'),loc= 'upper right',fontsize = 'small')
+# ax.add_artist(legend_1)
+# legend_2 = ax.legend(handles=(line1,line2,line3),
+#                      labels=(r'$\bar{y}$'+' = {}'.format(np.round(np.mean(st.session_state['y']),5)),
+#                              r'$\hat{\beta}_0$'+' = {}, '.format(beta_hat_0) + r'$\hat{\beta}_1$'+' = {}'.format(beta_hat_1),
+#                              r'$\beta_0$ = {}, '.format(beta_0) + r'$\beta_1$ = {}'.format(beta_1)),
+#                              loc = 'lower right',fontsize = 'small') # 추정량 비교
+
+# ax.set_title(option)
+# ax.set_xlim([-6,6])
+# ax.set_ylim([-20,20])
+# st.pyplot(fig)
